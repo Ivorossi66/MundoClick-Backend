8896d77187ac05ffa10f7cb31b8e88e26be309c6
Ivorossi66 <ivorossi66@gmail.com>
2025-12-16 10:45:26 -0300

    Subiendo estructura inicial del Backend

---
+++ database/config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error connecting to MongoDB');
+    }
+}
+
+module.exports = { dbConnection, };
+
---
+++ models/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+
+        //Conectar con la base de datos
+        this.connectarDB();
+
+        //Middlewares
+        this.middlewares();
+
+        //Función para las rutas
+        this.routes();
+
+    }
+
+    async connectarDB(){
+        await dbConnection();
+    }
+
+    middlewares() {
+        //CORS
+        this.app.use(cors());
+
+        //leer lo que el usuario envia por el cuerpo de la peticion
+        this.app.use(express.json());
+
+        //Definir la carpeta pública
+        this.app.use(express.static('public'));
+
+    }
+
+    routes() {
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server online on port: ${this.port}`);
+        })
+    }
+
+}
+
+module.exports = Server;
