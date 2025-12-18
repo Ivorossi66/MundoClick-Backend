4f7214b75a857ea4ae3b966a900d879d339ff4c2
Ivorossi66 <ivorossi66@gmail.com>
2025-12-17 21:25:39 -0300

    Config de schema categorias.js

--- models/categorias.js
+++ models/categorias.js
@@ -1,5 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const CategoriaSchema = Schema({
-    
+    nombre: { type: String, required: [true, 'El nombre es obligatorio'], unique: true },
+    descripcion: { type: String },
+    estado: { type: Boolean, required: true, default: true },
+    fechaRegistro: { type: Date, default: Date.now },
+    usuario: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true }, /* Se relaciona la categoria con un usuario */
 });
+
+module.exports = model('Categoria', CategoriaSchema);
